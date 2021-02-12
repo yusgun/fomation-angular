3181fccc5af63cae639dc32e0ccec32932568223
GUNER Yusuf <yusuf_guner@msn.com>
2021-02-12 13:53:23 +0100

    Ajout du cours sur les communications entre composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {​​ FormsModule, ReactiveFormsModule }​​ from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonneComponent } from './personne/personne.component';
+import { FormulaireDynamiqueComponent } from './formulaire-dynamique/formulaire-dynamique.component';
+import { ParentComponent } from './heritage/parent/parent.component';
+import { EnfantComponent } from './heritage/enfant/enfant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonneComponent,
+    FormulaireDynamiqueComponent,
+    ParentComponent,
+    EnfantComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
